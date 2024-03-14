8c0b78c3f6ce867a182f02c56e34416e119c0a46
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-14 13:31:06 +0200

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module for task 5
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function that print sum of a list
+    Parameter:
+        @input_list: list[float]
+
+    Return:
+        float: sum of input_list
+    """
+    return float(sum(input_list))
