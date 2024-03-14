76dfd671cd779325f10d910c4bb7cc5e854affba
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-14 12:46:14 +0200

    string rep

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for task 3
+"""
+
+
+def to_str(n: float) -> str:
+    """ Function to print string representation
+    of float
+    Parameters:
+    n: float
+    Return:
+    str: str representation of n
+    """
+    return str(n)
