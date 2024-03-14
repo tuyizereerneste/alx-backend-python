5a5d1f2b8e994f6b16639174e39a8bacd43394be
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-14 12:29:57 +0200

    concatenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module for task 1
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Fuction that concatenate strings
+    Parameters:
+    str1: string
+    str2: string
+
+    Return:
+    string: concatenation of str2 and str2
+    """
+    return str1 + str2
