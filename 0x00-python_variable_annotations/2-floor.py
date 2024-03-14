8ebd193ecb888e3769c8839a3d4e105131643f47
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-14 12:39:48 +0200

    floor of a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for tank 2
+"""
+
+
+def floor(n: float) -> int:
+    """ Function that returns floor
+    of a number
+
+    Parameter:
+    n: float
+
+    Return:
+    int: floor of float
+    """
+    return int(n)
