0fa8bacb2f05bcbf3281c2b106c75530f88e351a
tuyizereerneste <tuyizereernest@yahoo.com>
2024-04-08 19:13:02 +0200

    test github client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,3 +32,18 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_func.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self):
+        """Function that Tests the `_public_repos_url` property.
+        """
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
